5588a502aa961f03de350c85c7d03fa48a9b995d
100kgtrotila <hirodevi192@gmail.com>
2026-05-17 16:57:45 +0300

    feat: add points_used column to orders table and implement GrpcLoyaltyService for point tracking

---
+++ Cinema.Infrastructure/Migrations/20260517132802_AddPointsUsedToOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinema.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPointsUsedToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "points_used",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "points_used",
+                table: "orders");
+        }
+    }
+}
