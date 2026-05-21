2fca2a6a855b3c2df69911c397e1bffe2c1dabf3
100kgtrotila <hirodevi192@gmail.com>
2026-05-21 15:12:17 +0300

    feat: implement cinema core features including movie management, session scheduling, and user profile updates

---
+++ Cinema.Api/Mappings/MovieMappingConfig.cs
@@ -0,0 +1,32 @@
+using Cinema.Api.DTOs.Movies;
+using Cinema.Domain.Entities;
+using Mapster;
+
+namespace Cinema.Api.Mappings;
+
+/// <summary>
+/// Mapster type-adapter registrations for the Movies feature.
+/// Lives in Cinema.Api because the target types (DTOs) are presentation-layer concerns.
+/// </summary>
+public class MovieMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Movie, MovieDto>()
+            .Map(dest => dest.Id,             src => src.Id.Value)
+            .Map(dest => dest.Status,         src => (int)src.Status)
+            .Map(dest => dest.AgeRestriction, src => src.AgeRestriction)
+            .Map(dest => dest.Genres,         src => src.MovieGenres.Select(mg => mg.Genre.Name))
+            .Map(dest => dest.Cast,           src => src.Cast);
+
+        config.NewConfig<Movie, MovieListDto>()
+            .Map(dest => dest.Id,     src => src.Id.Value)
+            .Map(dest => dest.Status, src => (int)src.Status)
+            .Map(dest => dest.Genres, src => src.MovieGenres.Select(mg => mg.Genre.Name));
+
+        config.NewConfig<MovieCastMember, ActorDto>()
+            .Map(dest => dest.Name,     src => src.Name)
+            .Map(dest => dest.Role,     src => src.Role)
+            .Map(dest => dest.PhotoUrl, src => src.PhotoUrl);
+    }
+}
